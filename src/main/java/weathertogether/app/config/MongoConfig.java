5a2447314ea27e41020112f03e7156300d8802f1
Patricia <patricianicolentan@gmail.com>
2023-07-12 20:12:11 +0800

    Fix local setup, specifically env variables, and add code coverage in testing

--- src/main/java/weathertogether/app/config/MongoConfig.java
+++ src/main/java/weathertogether/app/config/MongoConfig.java
@@ -1,9 +1,7 @@
 package weathertogether.app.config;
 
-import com.google.appengine.api.utils.SystemProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.context.annotation.Bean;
@@ -11,16 +9,11 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import io.github.cdimascio.dotenv.Dotenv;
 
 
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
-    private final Environment environment;
-
-    @Autowired
-    public MongoConfig(Environment environment) {
-        this.environment = environment;
-    }
 
     @Override
     protected String getDatabaseName() {
@@ -30,9 +23,10 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
     @Override
     public MongoClient mongoClient() {
         // To run locally, add env vars for MongoDB credentials
-        String mongodbUsername = SystemProperty.environment.get("MONGODB_USERNAME");
-        String mongodbPassword = SystemProperty.environment.get("MONGODB_PASSWORD");
-        String formattedString = "mongodb+srv://" + username + ":" + password + "@weathertogether.dofg3ns.mongodb.net/";
+        Dotenv dotenv = Dotenv.configure().load();
+        String mongodbUsername = dotenv.get("MONGODB_USERNAME");
+        String mongodbPassword = dotenv.get("MONGODB_PASSWORD");
+        String formattedString = "mongodb+srv://" + mongodbUsername + ":" + mongodbPassword + "@weathertogether.dofg3ns.mongodb.net/";
         
         ConnectionString connectionString = new ConnectionString(formattedString);
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
